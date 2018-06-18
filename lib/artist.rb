f09366d423afea04a7ce774e3e0c28cea8a6047b
Richard Fenix <richard.fenix@gmail.com>
2018-06-18 03:39:40 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,57 +1,39 @@
 require 'pry'
 
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
   
   @@all = []
   
   def initialize(name)
     @name = name
     @songs = []
-    @@all << self
-  end
-  
-  def songs 
-    @songs
-#    Song.all.select {|song| song.artist == self}
-    
-  end
-  
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
-  
-  def save
-    @@all << self
-    @songs << self
+#    @@all << name
   end
   
   def self.all
     @@all
   end
+
+  def songs 
+    @songs 
+  end
+  
+  def add_song(song)
+    @songs << song
+  end
+  
+  def save
+    @@all << self
+  end
   
   def self.find_or_create_by_name(artist)
     if (self.all.find{|n| n.name == artist}.nil?)
-      self.new(artist)
+      self.new(artist).tap {|x| x.save}
     else
       self.all.find{|n| n.name == artist}
     end
   end
-
-  def self.add_song(song)
-    binding.pry
-    @songs << song.name
-    song.artist = self
-  end
-
-  # def self.find_or_create_by_name(artist)
-  #   if (self.all.nil?) || (self.all.find{|n| n.name == artist}.nil?)
-  #     self.new(artist)
-  #   else
-  #     self.all.find{|n| n.name == artist}
-  #   end
-  # end
 
   def print_songs
     @songs.each {|song| puts song.name}
@@ -59,6 +41,3 @@ class Artist
   
 end
 
-      # artist_1 = Artist.find_or_create_by_name("Michael Jackson")
-      # artist_2 = Artist.find_or_create_by_name("Michael Jackson")
-      # expect(artist_1).to eq(artist_2)
--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,7 +7,6 @@ class MP3Importer
   end
   
   def files
-#    list_of_files = Dir.entries(@path).map {|f| f[0, f.length-4]}.compact
     @files = Dir.entries(@path).select {|f| !File.directory? f}
   end
   
@@ -15,11 +14,5 @@ class MP3Importer
     self.files.each {|filename| Song.new_by_filename(filename)}
   end
 end
-  
-  
-      # Artist.class_variable_set("@@all",[])
-      # test_music_path = "./spec/fixtures/mp3s"
-      # music_importer = MP3Importer.new(test_music_path)
-      # music_importer.import
 
 
